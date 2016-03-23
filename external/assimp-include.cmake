850d65e04f5515bbfb336c1d9ade3ac3da5ee155
Tobias Zirr <tobias.zirr@kit.edu>
2016-03-23 13:55:53 +0100

    assimp configuration

---
+++ external/assimp-include.cmake
@@ -0,0 +1,8 @@
+# ASSIMP peripherals require old DirectX SDK, therefore disable from the start
+set(ASSIMP_BUILD_ASSIMP_TOOLS OFF CACHE BOOL "")
+set(ASSIMP_BUILD_SAMPLES OFF CACHE BOOL "")
+set(ASSIMP_BUILD_TESTS OFF CACHE BOOL "")
+set(ASSIMP_BUILD_STATIC_LIB ON CACHE BOOL "") #todo whats default?
+add_subdirectory(${EXTERNAL_DIR}/assimp ${EXTERNAL_BINARY_DIR}/assimp EXCLUDE_FROM_ALL)
+target_include_directories(assimp INTERFACE ${EXTERNAL_DIR}/assimp/include)
+list(APPEND EXTERNAL_TARGETS assimp)
