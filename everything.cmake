a8beb3a8615aab2aeb1dce8e69e3665b137743b8
Tobias Zirr <tobias.zirr@kit.edu>
2016-10-13 17:02:36 +0200

    minor cleanup

--- everything.cmake
+++ everything.cmake
@@ -1,4 +1,4 @@
-include(${CMAKE_CURRENT_LIST_DIR}/install-config.cmake)
+set(LIGHTER_MAKE_ALL_PROJECTS ON)
 include(${CMAKE_CURRENT_LIST_DIR}/global-flags.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/install-config.cmake)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR})
-include(${CMAKE_CURRENT_LIST_DIR}/global-flags-internal.cmake)
--- external/freetype-include.cmake
+++ external/freetype-include.cmake
@@ -3,6 +3,5 @@ add_subdirectory(${EXTERNAL_DIR}/freetype ${EXTERNAL_BINARY_DIR}/freetype EXCLUD
 if (LIGHTER_USE_CLEARTYPE)
 	target_compile_definitions(freetype PRIVATE FT_CONFIG_OPTION_SUBPIXEL_RENDERING TT_CONFIG_OPTION_SUBPIXEL_HINTING)
 endif()
-target_compile_definitions(freetype PUBLIC GLEW_STATIC)
 target_include_directories(freetype INTERFACE ${EXTERNAL_DIR}/freetype/include)
 list(APPEND EXTERNAL_TARGETS freetype)
